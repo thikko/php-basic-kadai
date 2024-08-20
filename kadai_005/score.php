89505d41329abbec1ce80ba54eaadbe15663dd14
thikko <aosaku706@icloud.com>
2024-08-21 03:03:50 +0900

    5章課題修正
    5章課題修正しました

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,4 +1,14 @@
 <?php
-$user_score=80+60+55+40+100+25+80+95+30+60;
-echo $user_score / 10;
+$score1=80;
+$score2=60;
+$score3=55;
+$score4=40;
+$score5=100;
+$score6=25;
+$score7=80;
+$score8=95;
+$score9=30;
+$score10=60;
+$user_score=$score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10;
+echo $user_score/10;
 ?>
