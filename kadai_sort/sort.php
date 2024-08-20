b901dcdfd8dfdea6497fb49900df1d1184bdbedc
thikko <aosaku706@icloud.com>
2024-08-21 02:30:00 +0900

    ソート課題
    ソート課題作成しました。

---
+++ kadai_sort/sort.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // ここにコードを書いていく
+        $nums = [15, 4, 18, 23, 10 ];
+        function sort_2way($array,$order){
+          if($order==true){
+            echo '昇順にソートします。<br>';
+            asort($array);
+          }else{
+            echo '降順にソートします。<br>';
+            arsort($array);
+          }
+          foreach($array as $value){
+            echo "{$value}<br>";
+          }
+        }
+
+        sort_2way($nums,true);
+        sort_2way($nums,false);
+
+        ?>
+    </p>
+</body>
+
+</html>
